70d376cad62f4f7bf887cc256568e5957f4d1463
Hakan <hknhzl@gmail.com>
2025-05-29 00:23:01 +0300

    garnet: vendorsetup.sh: Track camera stuff from codeberg

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,10 @@
 echo 'Starting to clone stuffs needed to build for garnet'
 
+# Camera Stuff
+echo 'Cloning Camera Stuff'
+rm -rf device/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/device_xiaomi_miuicamera-garnet.git -b lineage-22.2 device/xiaomi/miuicamera-garnet
+rm -rf vendor/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/vendor_xiaomi_miuicamera-garnet.git -b lineage-22.2 vendor/xiaomi/miuicamera-garnet
+
 # Agm
 echo 'Cloning Agm'
 rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/xiaomi-sm8450-marble/android_vendor_qcom_opensource_agm -b 15 hardware/qcom-caf/sm8450/audio/agm
