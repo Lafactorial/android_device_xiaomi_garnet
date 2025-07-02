b5a8160a8d8973a5df5528a8f4d4242172c25bd2
Lafactorial <hknhzl@gmail.com>
2025-07-02 11:33:13 +0300

    garnet: vendorsetup.sh: Track sm7435 kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,10 @@
 echo 'Starting to clone stuffs needed to build for garnet'
 
+# Kernel Stuff
+rm -rf kernel/xiaomi/sm7435 && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435.git -b lineage-22.2 kernel/xiaomi/sm7435
+rm -rf kernel/xiaomi/sm7435-modules && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435-modules.git -b lineage-22.2 kernel/xiaomi/sm7435-modules
+rm -rf kernel/xiaomi/sm7435-devicetrees && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435-devicetrees.git -b lineage-22.2 kernel/xiaomi/sm7435-devicetrees
+
 # Camera Stuff
 echo 'Cloning Camera Stuff'
 rm -rf device/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/device_xiaomi_miuicamera-garnet.git -b lineage-22.2 device/xiaomi/miuicamera-garnet
