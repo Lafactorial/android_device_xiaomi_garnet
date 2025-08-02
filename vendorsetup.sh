2e605d2f649fe4af77314f93c09f0282638929f0
Hakan <hknhzl@gmail.com>
2025-08-03 00:50:59 +0300

    garnet: vendorsetup.sh: Track my fork of sm7435 kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo 'Starting to clone stuffs needed to build for garnet'
 
 # Kernel Stuff
-rm -rf kernel/xiaomi/sm7435 && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435.git -b lineage-22.2 kernel/xiaomi/sm7435
+rm -rf kernel/xiaomi/sm7435 && git clone https://github.com/Lafactorial/android_kernel_xiaomi_sm7435.git -b lineage-23.0 kernel/xiaomi/sm7435
 rm -rf kernel/xiaomi/sm7435-modules && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435-modules.git -b lineage-22.2 kernel/xiaomi/sm7435-modules
 rm -rf kernel/xiaomi/sm7435-devicetrees && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435-devicetrees.git -b lineage-22.2 kernel/xiaomi/sm7435-devicetrees
 
@@ -9,11 +9,5 @@ rm -rf kernel/xiaomi/sm7435-devicetrees && git clone https://github.com/garnet-r
 echo 'Cloning Camera Stuff'
 rm -rf device/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/device_xiaomi_miuicamera-garnet.git -b lineage-22.2 device/xiaomi/miuicamera-garnet
 rm -rf vendor/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/vendor_xiaomi_miuicamera-garnet.git -b lineage-22.2 vendor/xiaomi/miuicamera-garnet
-
-# Hals
-rm -rf hardware/qcom-caf/sm8450/audio/graphservices && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audioreach-graphservices.git -b linega-23.0-caf-sm8450 hardware/qcom-caf/sm8450/audio/graphservices
-rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_agm -b linega-23.0-caf-sm8450 hardware/qcom-caf/sm8450/audio/agm
-rm -rf hardware/qcom-caf/sm8450/audio/primary-hal && git clone https://github.com/LineageOS/android_hardware_qcom_audio-ar -b linega-23.0-caf-sm8450 hardware/qcom-caf/sm8450/audio/primary-hal
-rm -rf hardware/qcom-caf/sm8450/audio/pal && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_arpal-lx -b linega-23.0-caf-sm8450 hardware/qcom-caf/sm8450/audio/pal
 
 echo 'Cloning process is completed, now its time for lunch'
