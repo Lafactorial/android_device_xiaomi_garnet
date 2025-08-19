765341acb7b16df74431ca521b753e32e9c3210b
Lafactorial <hknhzl@gmail.com>
2025-08-19 11:31:49 +0300

    garnet: vendorsetup.sh: Track lineage-22.2 branch of audio hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,4 +10,23 @@ echo 'Cloning Camera Stuff'
 rm -rf device/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/device_xiaomi_miuicamera-garnet.git -b lineage-22.2 device/xiaomi/miuicamera-garnet
 rm -rf vendor/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/vendor_xiaomi_miuicamera-garnet.git -b lineage-22.2 vendor/xiaomi/miuicamera-garnet
 
+# Agm
+echo 'Cloning Agm'
+rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_agm -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/agm
+
+# Graphservices
+echo 'Cloning Graphservices'
+rm -rf hardware/qcom-caf/sm8450/audio/graphservices && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audioreach-graphservices -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/graphservices
+
+# Pal
+echo 'Cloning Pal'
+rm -rf hardware/qcom-caf/sm8450/audio/pal && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_arpal-lx -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/pal
+
+# Primary
+echo 'Cloning Primary'
+rm -rf hardware/qcom-caf/sm8450/audio/primary-hal && git clone https://github.com/LineageOS/android_hardware_qcom_audio-ar -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/primary-hal
+
+# St-hal
+echo 'Cloning St'
+rm -rf vendor/qcom/opensource/audio-hal/st-hal-ar-legacy && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audio-hal_st-hal-ar -b lineage-22.2-legacy vendor/qcom/opensource/audio-hal/st-hal-ar-legacy
 echo 'Cloning process is completed, now its time for lunch'
