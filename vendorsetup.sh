844a3adfa0f51aebda04f15cff7f54c5551342d2
Hakan <hknhzl@gmail.com>
2025-08-29 15:05:33 +0300

    garnet: vendorsetup.sh: Track agm and pal from rising

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,21 +12,10 @@ rm -rf vendor/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafacto
 
 # Agm
 echo 'Cloning Agm'
-rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_agm -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/agm
-
-# Graphservices
-echo 'Cloning Graphservices'
-rm -rf hardware/qcom-caf/sm8450/audio/graphservices && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audioreach-graphservices -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/graphservices
+rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/RisingOS-Revived/android_vendor_qcom_opensource_agm -b sixteen hardware/qcom-caf/sm8450/audio/agm
 
 # Pal
 echo 'Cloning Pal'
-rm -rf hardware/qcom-caf/sm8450/audio/pal && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_arpal-lx -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/pal
+rm -rf hardware/qcom-caf/sm8450/audio/pal && git clone https://github.com/RisingOS-Revived/android_vendor_qcom_opensource_arpal-lx -b sixteen hardware/qcom-caf/sm8450/audio/pal
 
-# Primary
-echo 'Cloning Primary'
-rm -rf hardware/qcom-caf/sm8450/audio/primary-hal && git clone https://github.com/LineageOS/android_hardware_qcom_audio-ar -b lineage-22.2-caf-sm8450 hardware/qcom-caf/sm8450/audio/primary-hal
-
-# St-hal
-echo 'Cloning St'
-rm -rf vendor/qcom/opensource/audio-hal/st-hal-ar-legacy && git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audio-hal_st-hal-ar -b lineage-22.2-legacy vendor/qcom/opensource/audio-hal/st-hal-ar-legacy
 echo 'Cloning process is completed, now its time for lunch'
