f7964d06933cfc2dcc6202e2ca0279d588dd028f
Hakan <hknhzl@gmail.com>
2025-09-19 14:23:22 +0300

    garnet: vendorsetup.sh: Track agm and pal from Lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 echo 'Starting to clone stuffs needed to build for garnet'
 
 # Kernel Stuff
+echo 'Cloning Kernel Stuff'
 rm -rf kernel/xiaomi/sm7435 && git clone https://github.com/Lafactorial/android_kernel_xiaomi_sm7435.git -b lineage-23 kernel/xiaomi/sm7435
 rm -rf kernel/xiaomi/sm7435-modules && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435-modules.git -b lineage-22.2 kernel/xiaomi/sm7435-modules
 rm -rf kernel/xiaomi/sm7435-devicetrees && git clone https://github.com/garnet-random/android_kernel_xiaomi_sm7435-devicetrees.git -b lineage-22.2 kernel/xiaomi/sm7435-devicetrees
@@ -9,13 +10,5 @@ rm -rf kernel/xiaomi/sm7435-devicetrees && git clone https://github.com/garnet-r
 echo 'Cloning Camera Stuff'
 rm -rf device/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/device_xiaomi_miuicamera-garnet.git -b lineage-22.2 device/xiaomi/miuicamera-garnet
 rm -rf vendor/xiaomi/miuicamera-garnet && git clone https://codeberg.org/lafactorial/vendor_xiaomi_miuicamera-garnet.git -b lineage-22.2 vendor/xiaomi/miuicamera-garnet
-
-# Agm
-echo 'Cloning Agm'
-rm -rf hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/RisingOS-Revived/android_vendor_qcom_opensource_agm -b sixteen hardware/qcom-caf/sm8450/audio/agm
-
-# Pal
-echo 'Cloning Pal'
-rm -rf hardware/qcom-caf/sm8450/audio/pal && git clone https://github.com/RisingOS-Revived/android_vendor_qcom_opensource_arpal-lx -b sixteen hardware/qcom-caf/sm8450/audio/pal
 
 echo 'Cloning process is completed, now its time for lunch'
