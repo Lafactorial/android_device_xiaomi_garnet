92b9d43147448c46ce1a19817b04fa6e7c2d0dd1
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    garnet: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- device.mk
+++ device.mk
@@ -314,6 +314,7 @@ PRODUCT_PACKAGES += \
     FrameworkOverlayGarnet \
     FrameworkOverlayGarnetGLEsim \
     FrameworkOverlayGarnetJPEsim \
+    Launcher3DeviceOverlayGarnet \
     LineageSDKOverlayGarnet \
     LineageSettingsOverlayGarnet \
     LineageSystemUIOverlayGarnet \
