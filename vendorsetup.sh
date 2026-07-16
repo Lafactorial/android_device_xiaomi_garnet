f235dee033148a76fc0d297ea12b8bd61a7ddf97
Hakan <hknhzl@gmail.com>
2026-07-16 14:49:38 +0300

    garnet: vendorsetup.sh: Add signing keys cloning and execution
    Added commands to clone and run signing keys script.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,3 +14,9 @@ echo 'Cloning Camera Stuff'
 rm -rf device/xiaomi/garnet-miuicamera && git clone https://gitea.com/lafactorial/android_device_xiaomi_garnet-miuicamera.git -b sixteen-qpr2 device/xiaomi/garnet-miuicamera
 rm -rf vendor/xiaomi/garnet-miuicamera && git clone https://gitea.com/lafactorial/proprietary_vendor_xiaomi_garnet-miuicamera -b sixteen-qpr2 vendor/xiaomi/garnet-miuicamera
 echo 'Cloning process is completed, now its time for lunch'
+
+# Signig Stuff
+git clone https://github.com/Lafactorial/vendor_lineage-priv_keys -b ascp vendor/custom-priv/keys
+cd vendor/custom-priv/keys
+./keys.sh
+cd ../../../
