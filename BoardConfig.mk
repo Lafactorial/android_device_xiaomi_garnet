1eaaec8618d657cfc6f62f697db757285420dfbd
Lafactorial <hknhzl@gmail.com>
2026-09-23 08:16:29 +0300

    garnet: ASCP Bring Up
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,8 +199,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/lineage/sepolicy/libion/sepolicy.mk
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include device/custom/sepolicy/libion/sepolicy.mk
+include device/custom/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
