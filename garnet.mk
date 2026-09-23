6e844471e2c0b86afd37e4d9a771f7cf23b94b25
Lafactorial <hknhzl@gmail.com>
2026-09-23 08:16:29 +0300

    garnet: ASCP Bring Up
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- garnet.mk
+++ garnet.mk
@@ -10,12 +10,15 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
 
-PRODUCT_NAME := lineage_garnet
+ASCP_MAINTAINER := HaKaN
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_NAME := garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
